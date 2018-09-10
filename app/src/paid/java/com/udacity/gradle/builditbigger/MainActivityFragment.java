819dc0f73287309514258b0790657173ef9ce7d2
perfalcon <balaveer@gmail.com>
2018-09-10 19:17:25 -0400

    Completed the Paid and Free Flavor .. now completed all the tasks.

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,43 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.test.espresso.idling.CountingIdlingResource;
+import android.support.v4.app.Fragment;
+import android.support.v4.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.udacity.gradle.builditbigger.IdlingResource.BuildItBiggerIdlingResource;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        root.findViewById(R.id.btn_Joke).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                  new EndpointsAsyncTask ().execute (new Pair<Context, String> (view.getContext (),"Paid"));
+            }
+
+        });
+
+        return root;
+    }
+}
